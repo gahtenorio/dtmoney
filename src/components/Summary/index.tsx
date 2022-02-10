467765bb3ace4833bd1939390f99346565f43109
gahtenorio <jgst25@gmail.com>
2022-02-10 07:50:10 -0300

    componentization

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,35 @@
+import { Container } from './styles';
+
+import IncomeImg from '../../assets/income.svg';
+import OutcomeImg from '../../assets/outcome.svg';
+import totalImg from '../../assets/total.svg';
+
+export function Summary() {
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={IncomeImg} alt="Entradas" />
+        </header>
+        <strong>R$1000,00</strong>
+      </div>
+
+      <div>
+        <header>
+          <p>Saídas</p>
+          <img src={OutcomeImg} alt="Entradas" />
+        </header>
+        <strong>- R$500,00</strong>
+      </div>
+
+      <div className="highlight-background">
+        <header>
+          <p>Entradas</p>
+          <img src={totalImg} alt="Entradas" />
+        </header>
+        <strong>R$500,00</strong>
+      </div>
+    </Container>
+  );
+}
